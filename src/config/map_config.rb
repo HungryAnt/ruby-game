d0f81363cdd67f12fc4051568c6bb6e26c0c1ece
HungryAnt <ljsunlin@126.com>
2015-07-12 19:59:02 +0800

    update tails files

--- src/config/map_config.rb
+++ src/config/map_config.rb
@@ -1,14 +1,17 @@
-class MapConfig
-  DEFAULT_TAILS_PATH = 'resource/map/default_tails.txt'
 
-  hill_area = Area.new('map/hill/grass_wood_back.bmp', 'map/hill.ogg', DEFAULT_TAILS_PATH)
-  hill = Map.new([hill_area])
-
-  school_ground = Area.new('map/school/school_ground.bmp', 'map/school_ground.ogg', DEFAULT_TAILS_PATH)
-  school_lobby = Area.new('map/school/school_lobby.bmp', 'map/school_lobby.ogg', DEFAULT_TAILS_PATH)
-  school_room = Area.new('map/school/school_room.bmp', 'map/school_room.ogg', DEFAULT_TAILS_PATH)
-  school = Map.new [school_ground, school_lobby, school_room]
-
-  MapManager.add_map :hill, hill
-  MapManager.add_map :school, school
+def get_tail_path(name)
+  "resource/map/#{name}.txt"
 end
+
+DEFAULT_TAILS_PATH = get_tail_path('default_tails')
+
+hill_area = Area.new('map/hill/grass_wood_back.bmp', 'map/hill.ogg', get_tail_path('grass_wood_back'))
+hill = Map.new([hill_area])
+
+school_ground = Area.new('map/school/school_ground.bmp', 'map/school_ground.ogg', get_tail_path('school_ground'))
+school_lobby = Area.new('map/school/school_lobby.bmp', 'map/school_lobby.ogg', get_tail_path('school_lobby'))
+school_room = Area.new('map/school/school_room.bmp', 'map/school_room.ogg', get_tail_path('school_room'))
+school = Map.new [school_ground, school_lobby, school_room]
+
+MapManager.add_map :hill, hill
+MapManager.add_map :school, school
