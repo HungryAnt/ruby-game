76a7fe8c9c3e14b882325233b639fe8d375cb14a
HungryAnt <ljsunlin@126.com>
2015-07-14 22:54:44 +0800

    meta programming to cut down codes

--- src/models/role.rb
+++ src/models/role.rb
@@ -4,10 +4,19 @@ require_relative 'exp'
 require_relative 'package'
 
 class Role
+  class State
+    STANDING = :stand
+    WALKING = :walk
+    RUNNING = :run
+    EATING = :eat
+    HOLDING_FOOD = :hold_food
+  end
+
   include Location
   include Hp
   include Exp
 
+  attr_accessor :state
   attr_reader :package
 
   def initialize(x, y)
@@ -17,13 +26,11 @@ class Role
 
     @package = Package.new 100
     @eating_food = nil
+    @state = State::STANDING
   end
 
   def eat(food)
     @eating_food = food
-  end
-
-  def eating
-    !@eating_food.nil?
+    @state = State::EATING
   end
 end
--- src/modelviews/player_view_model.rb
+++ src/modelviews/player_view_model.rb
@@ -19,43 +19,38 @@ class PlayerViewModel
   end
 
   def init_animations
-    @anim_walk_laft = get_anim :walk_left
-    @anim_walk_right = get_anim :walk_right
-    @anim_walk_up = get_anim :walk_up
-    @anim_walk_down = get_anim :walk_down
+    # @anim_walk_left = get_anim :walk_left
+    # @anim_walk_right = get_anim :walk_right
+    # @anim_walk_up = get_anim :walk_up
+    # @anim_walk_down = get_anim :walk_down
+    #
+    # @anim_stand_left = get_anim :stand_left
+    # @anim_stand_right = get_anim :stand_right
+    # @anim_stand_up = get_anim :stand_up
+    # @anim_stand_down = get_anim :stand_down
+    #
+    # @anim_run_left = get_anim :run_left
+    # @anim_run_right = get_anim :run_right
+    # @anim_run_up = get_anim :run_up
+    # @anim_run_down = get_anim :run_down
+    #
+    # @anim_eat_left = get_anim :eat_left
+    # @anim_eat_right = get_anim :eat_right
+    # @anim_eat_up = get_anim :eat_up
+    # @anim_eat_down = get_anim :eat_down
 
-    @anim_stand_left = get_anim :stand_left
-    @anim_stand_right = get_anim :stand_right
-    @anim_stand_up = get_anim :stand_up
-    @anim_stand_down = get_anim :stand_down
-
-    @anim_run_left = get_anim :run_left
-    @anim_run_right = get_anim :run_right
-    @anim_run_up = get_anim :run_up
-    @anim_run_down = get_anim :run_down
-
-    @anim_eat_left = get_anim :eat_left
-    @anim_eat_right = get_anim :eat_right
-    @anim_eat_up = get_anim :eat_up
-    @anim_eat_down = get_anim :eat_down
+    %w(stand walk run eat).each do |state|
+      %w(left right up down).each do |direction|
+        self.instance_variable_set("@anim_#{state}_#{direction}",
+                                   get_anim("#{state}_#{direction}".to_sym))
+      end
+    end
 
     @current_anim = @anim_stand_down
   end
 
   def change_anim
-    anim = @anim_stand_down
-
-    act = ''
-
-    if @player.eating
-      act = 'eat'
-    elsif @standing
-      act = 'stand'
-    elsif @running
-      act = 'run'
-    else
-      act = 'walk'
-    end
+    state = @player.state.to_s
 
     direction = ''
     if is_direct_to Direction::LEFT
@@ -68,35 +63,12 @@ class PlayerViewModel
       direction = 'down'
     end
 
-    anim = self.instance_variable_get("@anim_#{act}_#{direction}")
-
-    # if @standing
-    #   if is_direct_to Direction::LEFT
-    #     anim = @anim_stand_left
-    #   elsif is_direct_to Direction::RIGHT
-    #     anim = @anim_stand_right
-    #   elsif is_direct_to Direction::UP
-    #     anim = @anim_stand_up
-    #   elsif is_direct_to Direction::DOWN
-    #     anim = @anim_stand_down
-    #   end
-    # else
-    #   if is_direct_to Direction::LEFT
-    #     anim = @running ? @anim_run_left : @anim_walk_laft
-    #   elsif is_direct_to Direction::RIGHT
-    #     anim = @running ? @anim_run_right : @anim_walk_right
-    #   elsif is_direct_to Direction::UP
-    #     anim = @running ? @anim_run_up : @anim_walk_up
-    #   elsif is_direct_to Direction::DOWN
-    #     anim = @running ? @anim_run_down : @anim_walk_down
-    #   end
-    # end
-
-    @current_anim = anim
+    @current_anim = self.instance_variable_get("@anim_#{state}_#{direction}")
   end
 
   def draw
-    #@image.draw_rot(@x, @y, ZOrder::Player, 0)
+    # @image.draw_rot(@x, @y, ZOrder::Player, 0)
+    # puts "@player.state: #{@player.state} @current_anim: #{@current_anim}"
     @current_anim.draw(@player.x, @player.y, ZOrder::Player)
   end
 
@@ -123,11 +95,13 @@ class PlayerViewModel
         @standing = true
         do_move(x, @player.y) unless map.tile_block? x, @player.y
         do_move(@player.x, y) unless map.tile_block? @player.x, y
+        @player.state = Role::State::STANDING if @standing
       else
         do_move(x, y)
       end
     else
       @standing = true
+      @player.state = Role::State::STANDING
     end
 
     have_a_rest if @standing
@@ -141,6 +115,7 @@ class PlayerViewModel
 
   def do_move(x, y)
     @standing = false
+    @player.state = @running ? Role::State::RUNNING : Role::State::WALKING
     @player.x = x
     @player.y = y
     @move_timestamp = Gosu::milliseconds
