44b6c997af55bccd8d1322f861df9c77d32accb4
HungryAnt <ljsunlin@126.com>
2015-09-03 20:46:30 +0800

    add hit message

---
+++ src/messages/hit_message.rb
@@ -0,0 +1,18 @@
+class HitMessage
+  attr_reader :user_id, :target_x, :target_y
+
+  def initialize(user_id, target_x, target_y)
+    @user_id, @target_x, @target_y = user_id, target_x, target_y
+  end
+
+  def to_json(*a)
+    {
+        type: 'hit_message',
+        data: {user_id: @user_id, target_x: @target_x, target_y: @target_y}
+    }.to_json(*a)
+  end
+
+  def self.json_create(map)
+    new(map['data']['user_id'], map['data']['target_x'].to_i, map['data']['target_y'].to_i)
+  end
+end
