27b3e66edd4de6ad634c1dfa739fca1372da0e07
HungryAnt <ljsunlin@126.com>
2015-12-24 23:34:22 +0800

    add new monsters anims

--- src/config/anim/monster_anim_config.rb
+++ src/config/anim/monster_anim_config.rb
@@ -27,6 +27,11 @@ lambda {
   # ¹¥»÷·¶Î§¶¯»­Ð§¹û
   new_centered_anim :monster_attack_effect, 'monster/effect/Effect_${num}.bmp', [3, 17], 220
 
+  new_monster_anims '0001', stand_down:[0, 11], move_down:[12, 19], attack_down:[20, 23],
+                    stand_up:[24, 27], move_up:[28, 35], attack_up:[36, 39],
+                    stand_hor:[40, 47], move_hor:[48, 51], attack_hor:[52, 55],
+                    capitulate:[56, 61]
+
   new_monster_anims '0002', stand_down:[0, 3], move_down:[4, 7], attack_down:[9, 12],
                     stand_up:[13, 16], move_up:[17, 21], attack_up:[22, 25],
                     stand_hor:[26, 29], move_hor:[30, 34], attack_hor:[35, 38],
@@ -37,4 +42,34 @@ lambda {
                     stand_up:[13, 16], move_up:[17, 20], attack_up:attack_0004,
                     stand_hor:[21, 24], move_hor:[25, 28], attack_hor:attack_0004,
                     capitulate:[29, 30]
+
+  attack_0005 = [17, 23]
+  new_monster_anims '0005', stand_down:[0, 7], move_down:[8, 15], attack_down:attack_0005,
+                    stand_up:[24, 27], move_up:[28, 31], attack_up:attack_0005,
+                    stand_hor:[32, 39], move_hor:[40, 47], attack_hor:attack_0005,
+                    capitulate:[48, 54]
+
+  attack_0006 = [8, 15]
+  new_monster_anims '0006', stand_down:[0, 3], move_down:[4, 7], attack_down:attack_0006,
+                    stand_up:[16, 19], move_up:[20, 23], attack_up:attack_0006,
+                    stand_hor:[24, 27], move_hor:[28, 31], attack_hor:attack_0006,
+                    capitulate:[32, 35]
+
+  attack_0007 = [6, 9]
+  new_monster_anims '0007', stand_down:[0, 3], move_down:[4, 5], attack_down:attack_0007,
+                    stand_up:[10, 13], move_up:[14, 15], attack_up:attack_0007,
+                    stand_hor:[16, 19], move_hor:[20, 21], attack_hor:attack_0007,
+                    capitulate:[22, 25]
+
+  attack_0008 = [8, 11]
+  new_monster_anims '0008', stand_down:[0, 3], move_down:[4, 7], attack_down:attack_0008,
+                    stand_up:[12, 15], move_up:[16, 19], attack_up:attack_0008,
+                    stand_hor:[20, 23], move_hor:[24, 25], attack_hor:attack_0008,
+                    capitulate:[26, 29]
+
+  attack_20000 = [11, 15]
+  new_monster_anims '20000', stand_down:[0, 4], move_down:[5, 10], attack_down:attack_20000,
+                    stand_up:[16, 20], move_up:[21, 26], attack_up:[27, 31],
+                    stand_hor:[32, 36], move_hor:[37, 42], attack_hor:attack_20000,
+                    capitulate:[43, 47]
 }.call
--- src/config/monster_config.rb
+++ src/config/monster_config.rb
@@ -7,6 +7,12 @@ lambda {
     MonsterTypeInfo.put monster_type_id, monster_type_info
   end
 
+  add.call '0001', '鳄鱼', 100
   add.call '0002', '野猫', 170
   add.call '0004', '变异鼠', 140
+  add.call '0005', '眼镜蛇', 100
+  add.call '0006', '秃鹫', 100
+  add.call '0007', '鼹鼠', 100
+  add.call '0008', '犀牛', 100
+  add.call '20000', '雪人', 100
 }.call
